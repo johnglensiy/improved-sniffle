fc9a21646c93bf6bc11cf232dd61f08342b1ae0b
John Glen Siy <johnglen_siy@berkeley.edu>
2026-03-16 17:12:42 -0700

    allow * cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
